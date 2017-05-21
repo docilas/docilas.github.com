3213d204501ac966037dd2d1bfd60d5612eb3f1a
Chester Kuo <docilas555@gmail.com>
2017-05-21 21:03:53 +0800

    Change to Pixi version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,259 +1,257 @@
-if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-
-var scene1, scene2;
-var composer, renderer, camera;
-var box;
-
-var bgWidth, bgHeight;
-var bgObj;
-var ax = 0,
-    ay = 0;
-
-
-// define Timeline animation
-var bgType1 = new TimelineMax({ repeat:-1, repeatDelay:1, yoyo:true, paused:true });
-var bgType2 = new TimelineMax({ paused:true, delay:3 });
-
-
-var turbVal = { val: 0.000001 };
-var turb = $('#noise feTurbulence')[0];
-var noiseEffect = new TimelineMax({ paused: true, repeat:-1, repeatDelay:3, onUpdate: function() {
-  turb.setAttribute('baseFrequency', '0 ' + turbVal.val);
-} });
-
-noiseEffect.to(turbVal, .2, { val: .7 });
-noiseEffect.to(turbVal, .4, { val: 0.000001 });
-
-//add event
-window.addEventListener( 'resize', onWindowResize, false );
-
-
-
-init();
-animate();
-
-
-
-
-
-// set canvas size
-function getBgSize() {
-  if( window.innerWidth >= window.innerHeight*2 ){
-    bgWidth = window.innerWidth;
-    bgHeight = window.innerWidth/2;
-  }
-  else{
-    bgWidth = window.innerHeight*2;
-    bgHeight = window.innerHeight;
-  }
-
-}
-
-function init() {
-  getBgSize();
-
-  camera = new THREE.PerspectiveCamera( 50, bgWidth / bgHeight, 1, 100 );
-  camera.position.z = 2;
-
-  scene1 = new THREE.Scene();
-  scene2 = new THREE.Scene();
-
-
-  box = new THREE.Mesh( new THREE.PlaneGeometry( 4, 2, 32 ));
-
-  scene1.add( box );
-
-
-  // geometry
-  var triangles = 1;
-  var instances = 3500;
-
-  var geometry = new THREE.InstancedBufferGeometry();
-
-  geometry.maxInstancedCount = instances;
-  var vertices = new THREE.BufferAttribute( new Float32Array( triangles * 3 * 3 ), 3 );
-
-  vertices.setXYZ( 0, 0.025, -0.025, 0 );
-  vertices.setXYZ( 1, -0.025, 0.025, 0 );
-  vertices.setXYZ( 2, 0, 0, 0.025 );
-
-  geometry.addAttribute( 'position', vertices );
-
-  var offsets = new THREE.InstancedBufferAttribute( new Float32Array( instances * 3 ), 3, 1 );
-
-  for ( var i = 0, ul = offsets.count; i < ul; i++ ) {
-    offsets.setXYZ( i, Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
-  }
-  geometry.addAttribute( 'offset', offsets );
-
-
-  var vector = new THREE.Vector4();
-
-  var orientationsStart = new THREE.InstancedBufferAttribute( new Float32Array( instances * 4 ), 4, 1 );
-
-  for ( var i = 0, ul = orientationsStart.count; i < ul; i++ ) {
-
-    vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
-    vector.normalize();
-
-    orientationsStart.setXYZW( i, vector.x, vector.y, vector.z, vector.w );
-  }
-
-  geometry.addAttribute( 'orientationStart', orientationsStart );
-
-  var orientationsEnd = new THREE.InstancedBufferAttribute( new Float32Array( instances * 4 ), 4, 1 );
-
-  for ( var i = 0, ul = orientationsEnd.count; i < ul; i++ ) {
-
-    vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
-    vector.normalize();
-
-    orientationsEnd.setXYZW( i, vector.x, vector.y, vector.z, vector.w );
-  }
-
-  geometry.addAttribute( 'orientationEnd', orientationsEnd );
-
-  // material
-  var material = new THREE.RawShaderMaterial( {
-    uniforms: {
-      time: { value: 18.0 },
-      sineTime: { value: 1.0 }
-    },
-    vertexShader: document.getElementById( 'vertexShader' ).textContent,
-    transparent: true
-  } );
-
-  var mesh = new THREE.Mesh( geometry, material );
-  scene2.add( mesh );
-
-  // end
-  renderer = new THREE.WebGLRenderer( { antialias: false } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( bgWidth, bgHeight );
-  renderer.autoClear = false;
-
-  $('#bg').append( renderer.domElement )
-
-  //mask
-  var clearPass = new THREE.ClearPass();
-  var clearMaskPass = new THREE.ClearMaskPass();
-
-  var maskPass1 = new THREE.MaskPass( scene1, camera );
-  var maskPass2 = new THREE.MaskPass( scene2, camera );
-
-  var texture1 = new THREE.TextureLoader().load( 'assets/images/bg1.jpg' );
-  var texture2 = new THREE.TextureLoader().load( 'assets/images/bg1b.jpg' );
-
-  var texturePass1 = new THREE.TexturePass( texture1 );
-  var texturePass2 = new THREE.TexturePass( texture2, .85 );
-
-
-  var outputPass = new THREE.ShaderPass( THREE.CopyShader );
-  outputPass.renderToScreen = true;
-
-  var parameters = {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    format: THREE.RGBFormat,
-    stencilBuffer: true
-  };
-
-  var renderTarget = new THREE.WebGLRenderTarget( bgWidth, bgHeight, parameters );
-
-  composer = new THREE.EffectComposer( renderer, renderTarget );
-  composer.addPass( clearPass );
-  composer.addPass( maskPass1 );
-  composer.addPass( texturePass1 );
-  composer.addPass( clearMaskPass );
-  composer.addPass( maskPass2 );
-  composer.addPass( texturePass2 );
-  composer.addPass( clearMaskPass );
-  composer.addPass( outputPass );
-
-  bgObj = scene2.children[0];
-
-  bgType1.to( bgObj.material.uniforms.time, 8, {value: 10, ease: Circ.easeInOut });
-
-  bgType2.to( bgObj.material.uniforms.time, 5, {value: -4, ease: Circ.easeInOut });
-
-  setBgAni(1);
-  $('.sec_home').addClass('show');
-
-}
-
-
-
-
-// set BG Animation type
-function setBgAni(type) {
-
-  if (type==1){
-    bgType2.stop();
-    TweenMax.to( bgObj.material.uniforms.time, 18, {value: 18, ease: Circ.easeInOut, onComplete:cont });
+$(function() {
+  let bg_z = -10,
+      bg_r = Math.abs(bg_z / 10);
+  let count = 0;
+  const power = 7;
+  let workIndex = -1;
+  var workPrev, workNow;
+  const workNum = $('.sec_work .work').length;
+  let homeView = true,
+      workAni = false;
+
+
+  // pixi background
+  const app = new PIXI.Application(window.innerWidth, window.innerHeight);
+  $('#bg').append(app.view);
+
+  container = new PIXI.Container();
+
+  const displacementSprite = PIXI.Sprite.fromImage('assets/images/filter.png');
+  const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+  displacementSprite.anchor.set(0.5);
+  displacementSprite.position= {x: app.renderer.width / 2, y: app.renderer.height / 2};
+
+  const bg = PIXI.Sprite.fromImage('assets/images/bg.jpg');
+
+  setBgSize();
+
+
+
+  bg.anchor.set(0.5);
+  bg.position= {x: app.renderer.width / 2, y: app.renderer.height / 2};
+
+
+  // text
+  const text1 = new PIXI.Text('CHIH YUAN KUO', {
+    fontWeight: 500,
+    fontSize: 100,
+    fontFamily: 'Impact',
+    fill: '#333',
+    align: 'center',
+    strokeThickness: 0,
+    letterSpacing: 10
+  });
+
+  text1.anchor.set(.5);
+  text1.blendMode = PIXI.BLEND_MODES.ADD;
+  text1.position= {x: app.renderer.width / 2, y: app.renderer.height / 2};
+
+
+  const textLft = new PIXI.Text('CHIH YUAN KUO', {
+    fontWeight: 500,
+    fontSize: 100,
+    fontFamily: 'Impact',
+    fill: '#FC577D',
+    align: 'center',
+    strokeThickness: 0,
+    letterSpacing: 10
+  });
+  textLft.anchor.set(.5);
+  textLft.blendMode = PIXI.BLEND_MODES.ADD;
+  textLft.position= {x: app.renderer.width / 2, y: app.renderer.height / 2};
+  textLft.alpha = 0;
+
+  const textRig = new PIXI.Text('CHIH YUAN KUO', {
+    fontWeight: 500,
+    fontSize: 100,
+    fontFamily: 'Impact',
+    fill: '#54F3B7',
+    align: 'center',
+    strokeThickness: 0,
+    letterSpacing: 10
+  });
+  textRig.anchor.set(.5);
+  textRig.blendMode = PIXI.BLEND_MODES.ADD;
+  textRig.position= {x: app.renderer.width / 2, y: app.renderer.height / 2};
+  textRig.alpha = 0;
+
+  container.addChild(bg, textLft, textRig, text1);
+
+  container.filters = [displacementFilter];
+  app.stage.addChild(container, displacementSprite);
+
+
+
+
+
+  app.ticker.add(function(delta) {
+    count += 0.1;
+    displacementSprite.rotation += 0.005;
+    displacementSprite.scale.x = 2 + Math.sin(count) * 0.2;
+    displacementSprite.scale.y = 2 + Math.cos(count) * 0.2;
+
+    if (homeView) {
+      text1.scale.x = 1 + Math.cos(count) * 0.003;
+      text1.scale.y = 1 + Math.sin(count) * 0.006;
+    }
+
+
+  });
+
+  // title animation
+  const tl = new TimelineMax({ repeat: 1, repeatDelay: .5, paused: true});
+    tl.to( textLft, .05, {alpha: .4, x: text1.x -power, y: text1.y +power, ease:Circ.easeOut })
+      .to( textRig, .05, {alpha: .4, x: text1.x +power, y: text1.y -power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x -power, y: text1.y -power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x +power, y: text1.y +power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x +power, y: text1.y +power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x -power, y: text1.y -power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x +power, y: text1.y -power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x -power, y: text1.y +power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x, y: text1.y, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x, y: text1.y, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x -power, y: text1.y +power, ease:Circ.easeOut }, "+=.2")
+      .to( textRig, .05, {x: text1.x +power, y: text1.y -power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x -power, y: text1.y -power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x +power, y: text1.y +power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x +power, y: text1.y +power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x -power, y: text1.y -power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x +power, y: text1.y -power, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x -power, y: text1.y +power, ease:Circ.easeOut }, "-=.05")
+      .to( textLft, .05, {x: text1.x, y: text1.y, alpha: 0.1, ease:Circ.easeOut })
+      .to( textRig, .05, {x: text1.x, y: text1.y, alpha: 0.1, ease:Circ.easeOut }, "-=.05");
+
+
+  init();
+
+  function init() {
+    TweenMax.fromTo('#bg canvas', 2, {z:200}, {z:bg_z, ease: Sine.easeOut});
+    TweenMax.fromTo(text1, 2, {alpha: 0}, {alpha: 1, delay: .5, ease: Sine.easeOut, onComplete:glitchOn});
+
+    TweenMax.set('#menu', {className:'+=active', delay:4});
+    // TweenMax.set('#menu', {className:'-=active', delay:6.5});
 
 
   }
-  else if(type==2){
-    bgType1.stop();
-    bgType2.play();
+  function setBgSize() {
+
+    if ( window.innerWidth *.5625 <  window.innerHeight ) {
+      bg.scale.set( window.innerHeight / .5625 / 1920 );
+    }
+    else{
+      bg.scale.set( window.innerWidth * .5625 / 1080 );
+    }
+    TweenMax.set( '.sec_work', {y: -window.innerHeight* (workIndex + 1)});
+
   }
 
-  function cont() {
-    bgType1.play();
+
+  function glitchOn(){
+    tl.restart();
   }
 
-}
+  function workNext(way) {
+    workAni = true;
+    homeView = false;
+    TweenMax.set('.btn-discover', {className:'+=hide'});
+
+    if (way=='up') {
+      workIndex--;
+      workNow = $('.sec_work .work').eq(workIndex);
+      workPrev = $('.sec_work .work').eq(workIndex+1);
+    }
+    else{
+      workIndex++;
+      workNow = $('.sec_work .work').eq(workIndex);
+      workPrev = $('.sec_work .work').eq(workIndex-1);
+    }
+    TweenMax.to( '.sec_work', .75, {y: -window.innerHeight* (workIndex + 1), ease: Circ.easeInOut, onComplete: aniEnd});
+
+    if (workIndex !== 0) {
+      TweenMax.fromTo( workPrev.find('.img'), .5, { z:10 }, { z: -20, ease: Circ.easeOut});
+      TweenMax.fromTo( workPrev.find('.info'), .5, { z:40 }, { z: -15, ease: Circ.easeOut});
+    }
+    workPrev.removeClass('active');
+    workNow.addClass('active');
+    TweenMax.fromTo( workNow.find('.img'), .75, { z:-20 }, { z: 10, delay:.5, ease: Circ.easeOut});
+    TweenMax.fromTo( workNow.find('.info'), .75, { z:-15 }, { z: 40, delay:.5, ease: Circ.easeOut});
+
+    if (workIndex == -1) {
+      // back to home
+      homeView = true;
+      bg_z = -10;
+      TweenMax.to('#bg canvas', .75, {z:bg_z, ease:Sine.easeOut});
+      TweenMax.set('.btn-scroll', {className:'-=hide'});
+      tl.restart();
+    }
+    else{
+      // to work list
+      TweenMax.set('.btn-discover', {className:'-=hide', delay:.2});
+
+      if(workIndex == 0){
+        // to work list
+        bg_z = -50;
+        TweenMax.to('#bg canvas', .75, {z:bg_z, ease:Sine.easeOut});
+        TweenMax.set('.btn-scroll', {className:'+=hide'});
+
+      }
+    }
+    bg_r = Math.abs(bg_z / 10);
+
+  }
+  function aniEnd() {
+    workAni = false;
+  }
 
 
-function onWindowResize() {
-  getBgSize();
+  $(window).resize(setBgSize);
+  // mouse event
 
-  camera.aspect = bgWidth / bgHeight;
-  camera.updateProjectionMatrix();
+  $('body').on('mousemove touchmove', (e)=>{
 
-  renderer.setSize( bgWidth, bgHeight );
+    let ax = e.clientX / window.innerWidth -.5,
+        ay = e.clientY / window.innerHeight - .5;
 
-}
+    TweenMax.to( '#bg canvas', .5, { rotationX: -ay*7/bg_r, rotationY: ax*7/bg_r, x: -ax*10/bg_r+'%', y:-ay*10/bg_r+'%' });
 
-$('.sec_home').on('mousemove', function(e) {
-  ax = -(window.innerWidth / 2 - e.clientX)/ window.innerWidth;
-  ay = (window.innerHeight / 2 - e.clientY) / window.innerHeight;
-})
-
-function animate() {
-  requestAnimationFrame( animate );
-  var time = performance.now();
+    if (!homeView) {
+      TweenMax.to( workNow.find('.img'), .75, { rotationX: -ay*5, rotationY: ax*5, x:-ax*5+'%', y:-ay*5+'%' });
+      TweenMax.to( workNow.find('.info'), 1, { rotationX: -ay*7, rotationY: ax*7, x:-ax*10+'%', y:-ay*10+'%' });
+    }
 
 
 
-  bgObj.rotation.z = time * 0.0002;
-  bgObj.rotation.y =  ax||0;
-  bgObj.rotation.x =  ay||0;
+  });
+  $('main').on('mousewheel', function(e) {
 
-  // shake glitch
-  // bgObj.position.x = .02 * Math.sin( time );
+    if ( !workAni ) {
 
-  bgObj.material.uniforms.sineTime.value = Math.sin( bgObj.material.uniforms.time.value * 0.05 );
+      if (e.deltaY > 0) {
+        //wheel up
+        if (workIndex == -1) {
+          // home
+          return false;
+        }
+        else{
+          workNext('up');
+        }
+      }
+      else{
+        //wheel down
+        if (workIndex== $('.sec_work .work').length -1 ) {
+          //last one
+          return false;
+        }
+        else{
+          workNext('down');
+        }
+      }
+    }
+  });
 
-  composer.render( );
-}
+  $('.btn-scroll').on('click', ()=>{ workNext('down'); });
 
 
 
-// work hover mouser event
-$('.sec_work .block').on('mouseenter', function() {
-  var $this = $(this);
-  TweenMax.set($this, {className:'+=do_glitch'});
-  TweenMax.set($this, {className:'-=do_glitch', delay: 1.5});
-  noiseEffect.restart();
 });
-
-// $('.sec_work').on('mouseenter', function() {
-//   setBgAni(1);
-// });
-
-
-
-
-
-
